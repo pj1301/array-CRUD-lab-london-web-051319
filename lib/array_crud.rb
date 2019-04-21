b049c06e2671f42ddb7e64aca523439a6f721c44
James Hayward <james.hayward6@icloud.com>
2019-04-21 20:14:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,11 +12,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.unshift(element)
+  array.unshift("#{element}"")
 end
 
 def add_element_to_start_of_array(array, element)
-  array.push("element")
+  array.push(element)
 end
 
 def remove_element_from_end_of_array(array)
