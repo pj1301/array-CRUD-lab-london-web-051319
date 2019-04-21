736e0652f43b456624aaabe225cec6e683d4df01
James Hayward <james.hayward6@icloud.com>
2019-04-21 20:09:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  comida.unshift("queso")
+  array.unshift("element")
 end
 
 def add_element_to_start_of_array(array, element)
