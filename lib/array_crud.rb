99c6d09002eb9164b981901a2dbd5743c2d6ab93
James Hayward <james.hayward6@icloud.com>
2019-04-21 20:04:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+ create_an_array = [
+   "res",
+   "cerdo",
+   "pollo",
+   "cordero"
+ ]
 end
 
 def add_element_to_end_of_array(array, element)
